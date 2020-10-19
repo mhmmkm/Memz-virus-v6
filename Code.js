35654ce7a3c50ba2cb8f19cfa173c43e71f81833
mhmmkm <72941240+mhmmkm@users.noreply.github.com>
2020-10-19 15:03:30 -0600

    Create Code.js

---
+++ Code.js
@@ -0,0 +1,130 @@
+
+
+(function theLoop (i) {
+  setTimeout(function () {
+
+setTimeout(function(){
+$exe("fx invert")
+$exe("layer /c/sys/skins/w93/error.png")
+$alert({draggable: !1, header: !1, center: !1, title:"'Best way to kill myself' - Bing search", icon: "/c/sys/skins/w93/apps/catexplorer.png", msg: "LMAO", btnOk: "WHAT?", onclose:function(){$alert.error('Looks like ur pc is dead, kiled by memz virus lol.')}})
+
+}, 1000); 
+
+
+setTimeout(function(){
+$alert({draggable: !1, header: !1, center: !1, title:"'Best way to kill myself' - Bing search", icon: "/c/sys/skins/w93/apps/catexplorer.png", msg: "LMAO", btnOk: "WHAT?", onclose:function(){$alert.error('Looks like ur pc is dead, kiled by memz virus lol.')}})
+
+$exe("fx invert")
+$exe("layer /c/sys/skins/w93/error.png")
+
+}, 2000); 
+
+setTimeout(function(){
+$exe("fx invert")
+$exe("layer /c/sys/skins/w93/error.png")
+$alert({draggable: !1, header: !1, center: !1, title:"'Best way to kill myself' - Bing search", icon: "/c/sys/skins/w93/apps/catexplorer.png", msg: "LMAO", btnOk: "WHAT?", onclose:function(){$alert.error('Looks like ur pc is dead, kiled by memz virus lol.')}})
+
+}, 3000); 
+
+setTimeout(function(){
+$exe("fx invert")
+$exe("layer /c/sys/skins/w93/error.png") 
+$alert({draggable: !1, header: !1, center: !1, title:"'Best way to kill myself' - Bing search", icon: "/c/sys/skins/w93/apps/catexplorer.png", msg: "LMAO", btnOk: "WHAT?", onclose:function(){$alert.error('Looks like ur pc is dead, kiled by memz virus lol.')}})
+}, 4000); 
+
+setTimeout(function(){
+$exe("fx invert")
+$exe("layer /c/sys/skins/w93/error.png") 
+$exe("layer /c/files/images/icons/yea.png")
+$alert({draggable: !1, header: !1, center: !1, title:"'Best way to kill myself' - Bing search", icon: "/c/sys/skins/w93/apps/catexplorer.png", msg: "LMAO", btnOk: "WHAT?", onclose:function(){$alert.error('Looks like ur pc is dead, kiled by memz virus lol.')}})
+
+}, 5000); 
+
+setTimeout(function(){
+$exe("fx invert")
+$exe("layer /c/sys/skins/w93/error.png") 
+$exe("layer /c/files/images/icons/yea.png")
+$alert({draggable: !1, header: !1, center: !1, title:"'Best way to kill myself' - Bing search", icon: "/c/sys/skins/w93/apps/catexplorer.png", msg: "LMAO", btnOk: "WHAT?", onclose:function(){$alert.error('Looks like ur pc is dead, kiled by memz virus lol.')}})
+
+}, 6000); 
+
+setTimeout(function(){
+$exe("fx invert")
+$exe("layer /c/sys/skins/w93/error.png")
+$exe("layer /c/files/images/icons/yea.png")
+$alert({draggable: !1, header: !1, center: !1, title:"'Best way to kill myself' - Bing search", icon: "/c/sys/skins/w93/apps/catexplorer.png", msg: "LMAO", btnOk: "WHAT?", onclose:function(){$alert.error('Looks like ur pc is dead, kiled by memz virus lol.')}})
+
+}, 7000); 
+
+
+setTimeout(function(){
+$exe("layer /c/files/images/icons/no.png")
+$exe("whatif","lisa","clippy")
+$exe("fx invert")
+$alert({draggable: !1, header: !1, center: !1, title:"'Best way to kill myself' - Bing search", icon: "/c/sys/skins/w93/apps/catexplorer.png", msg: "LMAO", btnOk: "WHAT?", onclose:function(){$alert.error('Looks like ur pc is dead, kiled by memz virus lol.')}})
+
+}, 8000); 
+
+setTimeout(function(){
+$exe("layer /c/files/images/icons/no.png")
+
+$exe("fx invert")
+$alert({draggable: !1, header: !1, center: !1, title:"'Best way to kill myself' - Bing search", icon: "/c/sys/skins/w93/apps/catexplorer.png", msg: "LMAO", btnOk: "WHAT?", onclose:function(){$alert.error('Looks like ur pc is dead, kiled by memz virus lol.')}})
+
+}, 9000); 
+
+setTimeout(function(){
+$exe("layer /c/files/images/icons/no.png")
+
+$exe("fx invert")
+$alert({draggable: !1, header: !1, center: !1, title:"'Best way to kill myself' - Bing search", icon: "/c/sys/skins/w93/apps/catexplorer.png", msg: "LMAO", btnOk: "WHAT?", onclose:function(){$alert.error('Looks like ur pc is dead, kiled by memz virus lol.')}})
+}, 10000); 
+
+setTimeout(function(){
+$exe("layer /c/files/images/icons/no.png")
+
+$exe("fx invert")
+$alert({draggable: !1, header: !1, center: !1, title:"'Best way to kill myself' - Bing search", icon: "/c/sys/skins/w93/apps/catexplorer.png", msg: "LMAO", btnOk: "WHAT?", onclose:function(){$alert.error('Looks like ur pc is dead, kiled by memz virus lol.')}})
+
+}, 11000); 
+
+setTimeout(function(){
+$exe("layer /c/files/images/icons/no.png")
+
+$exe("fx invert")
+$alert({draggable: !1, header: !1, center: !1, title:"'Best way to kill myself' - Bing search", icon: "/c/sys/skins/w93/apps/catexplorer.png", msg: "LMAO", btnOk: "WHAT?", onclose:function(){$alert.error('Looks like ur pc is dead, kiled by memz virus lol.')}})
+
+}, 12000); 
+
+setTimeout(function(){
+  
+$exe("fx invert")
+$alert({draggable: !1, header: !1, center: !1, title:"'Best way to kill myself' - Bing search", icon: "/c/sys/skins/w93/apps/catexplorer.png", msg: "LMAO", btnOk: "WHAT?", onclose:function(){$alert.error('Looks like ur pc is dead, kiled by memz virus lol.')}})
+
+}, 13000); 
+
+setTimeout(function(){
+
+$exe("fx invert")
+$alert({draggable: !1, header: !1, center: !1, title:"'Best way to kill myself' - Bing search", icon: "/c/sys/skins/w93/apps/catexplorer.png", msg: "LMAO", btnOk: "WHAT?", onclose:function(){$alert.error('Looks like ur pc is dead, kiled by memz virus lol.')}})
+
+
+}, 14000); 
+
+          if (--i) {
+      theLoop(i);
+    }
+  }, 15000); //Here u can customize the delay (1000=1sec)
+})(2); //Here u can change how many times a file is created
+    
+$exe("textarea");$window.current.el.body.firstChild.value="YOUR COMPUTER HAS BEEN FUCKED BY THE MEMZ TROJAN (not really).\nYour computer won't boot up again, so use it as long as you can!\n:D\nTrying to kill MEMZ will cause your system to be destroyed instantly, so don't try it. Made by mkm :D\n";$window.current.changeSize({width: 750, height: 400});$alert({draggable: !1, header: !1, center: !1, title: "'Best way to kill myself' - Bing search", icon: "/c/sys/skins/w93/apps/catexplorer.png", msg: "Reboot button", btnOk: "Reboot", onclose:function(){document.write('<h1>Your computer has been destroyed by MEMZ trojan. Enjoy the Nyan Cat...</h1><img width="600" src="https://i.imgur.com/vuCXT9O.gif"><iframe width="30" height="32" src="https://www.youtube.com/embed/QH2-TGUlwu4" frameborder="0" allow="accelerometer; autoplay; loop; encrypted-media; gyroscope; picture-in-picture" allowfullscreen autoplay loop></iframe>');} })
+
+$alert({draggable: !1, header: !1, center: !1, title: "'Best way to kill myself' - Bing search", icon: "/c/sys/skins/w93/apps/catexplorer.png", msg: "PC Destroy 100%?(do this last plz)", btnOk: "yes", onclose:function(){$loader.script("https://raw.githack.com/mhmmkm/Yhingy/main/virus.js")}})
+
+
+
+
+$notif('Windows Defender has detected a Virus on your system. Press reboot to contain.')
+
+$alert.info('Virus detected')
+//Memz virus made possible by mkm or mkmhome
