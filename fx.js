ee666acea45189a51319de09c1b3e7c4ca8c676d
mhmmkm <72941240+mhmmkm@users.noreply.github.com>
2020-10-21 11:23:01 -0600

    Add files via upload

---
+++ fx.js
@@ -0,0 +1,16 @@
+(function theLoop (i) {
+  setTimeout(function () {
+
+$window.current.destroy()
+
+          if (--i) {
+      theLoop(i);
+    }
+  }, 1000); //Here u can customize the delay (1000=1sec)
+})(1000); //Here u can change how many times
+
+$exe("fx square")
+
+$alert({draggable: !1, header: !1, center: !1, title:"'s.go' - Bing search", icon: "/c/sys/skins/w93/apps/catexplorer.png", msg: "LMAO", btnOk: "WHAT?", onclose:function(){$exe("fx none")}})
+
+$alert({draggable: !1, header: !1, center: !1, title:"'sf' - Bing search", icon: "/c/sys/skins/w93/apps/catexplorer.png", msg: "LMAO", btnOk: "WHAT?", onclose:function(){$alert({draggable: !1, header: !1, center: !1, title:"'sf' - Bing search", icon: "/c/sys/skins/w93/apps/catexplorer.png", msg: "LMAO", btnOk: "WHAT?", onclose:function(){$alert.error('Looks like ur pc is dead, kiled by memz virus lol.')}})}})
